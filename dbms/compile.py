db0cee49a85276129ba20458cdd63a60f4b0739b
Phoenix009 <jaideepmore307@gmail.com>
2022-01-27 14:17:25 +0530

    Final

--- dbms/compile.py
+++
@@ -1,8 +0,0 @@
-with open('compiled.txt', 'a') as output:
-    with open('topics.txt') as names:
-        for name in names:
-            name = name.strip()
-            with open(name) as target:
-                for line in target:
-                    output.write(line)
-
